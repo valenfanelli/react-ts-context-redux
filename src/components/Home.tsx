aab49c58d17c13e66e94ef08909a534800b64d7b
valenfanelli <valentinafanelliaddiechi@hotmail.com>
2025-02-06 09:43:18 -0300

    Update error language

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   
   return <>
     <header>
-        <h1 className="title">React projects with useReducer, context API y Redux</h1>
+        <h1 className="title">React projects with useReducer, context API & Redux</h1>
     </header>
     <p className="title">Choose the example you want to see in the navigation bar down here</p>
 </>
